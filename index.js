5542d280ae304c3d16f8ab52a0f506dea2c37cbf
safinxr <safinxr@gmail.com>
2023-10-21 12:38:24 +0600

    get car details by id added

--- index.js
+++ index.js
@@ -2,21 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config()
-// require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json())
 
-const alu = "a";
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@trigun-cars.tfbanll.mongodb.net/?retryWrites=true&w=majority`;
-
-
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -30,13 +26,49 @@ async function run() {
         await client.connect();
 
 
+        const brandDB = client.db("trigun-db").collection("brandDB");
+        const carsDB = client.db("trigun-db").collection("carsDB");
+
+        app.get('/brands', async (req, res) => {
+            const cursor = brandDB.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get('/display', async (req, res) => {
+            const cursor = carsDB.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get('/display/:brand', async (req, res) => {
+            const brand = req.params.brand;
+            
+            const query = {brand_name: brand };
+
+            const result = await carsDB.find(query).toArray();
+            res.send(result);
+        })
+
+        app.get('/cardata/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await carsDB.findOne(query);
+            res.send(result);
+        })
+
+        app.post('/cardata', async (req, res) => {
+            const carData = req.body;
+            const result = await carsDB.insertOne(carData);
+            res.send(result);
+        })
 
 
 
         await client.db("admin").command({ ping: 1 });
         console.log("✅✅✅✅ connected to MongoDB! ✅✅✅✅");
     } finally {
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
